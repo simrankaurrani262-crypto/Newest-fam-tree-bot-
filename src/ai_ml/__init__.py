ba3708a3b881cdac67fc3d6168704f97b0f24336
simrankaurrani262-crypto <simrankaurrani262@gmail.com>
2026-03-25 14:06:44 +0530

    Add files via upload

--- src/ai_ml/__init__.py
+++ src/ai_ml/__init__.py
@@ -1,1 +1,12 @@
+"""AI/ML Module for Fam Tree Bot"""
+from src.ai_ml.nlp.processor import NLPProcessor
+from src.ai_ml.recommendation.engine import RecommendationEngine
+from src.ai_ml.fraud_detection.detector import FraudDetector
+from src.ai_ml.image_generation.generator import ImageGenerator
 
+__all__ = [
+    "NLPProcessor",
+    "RecommendationEngine", 
+    "FraudDetector",
+    "ImageGenerator"
+]
